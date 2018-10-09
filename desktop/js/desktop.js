506e1dfac6776073cd8db451204ce864448da0e4
Max-Loo <1124843223@qq.com>
2018-10-09 19:50:50 +0800

    page3 v1.0

--- desktop/js/desktop.js
+++ desktop/js/desktop.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor:['#FF9966','#5c7ff1','#009966','#9933CC','#CC9966'], //控制每个section的背景颜色
+		licenseKey:'OPEN-SOURCE-GPLV3-LICENSE',
+		sectionsColor:['#FF9966','#5c7ff1','#00B2B2','#9933CC','#CC9966'], //控制每个section的背景颜色
 		css3: true,
 		scrolllingSpeed:700,
 		anchors:['page1','page2','page3','page4'],
@@ -15,5 +16,6 @@ $(document).ready(function() {
 		navigationColor:'#fff', //项目导航的颜色
 		slidesNavigation:true,
 		loopHorizontal: true,
-	});
+
+		});
 });
